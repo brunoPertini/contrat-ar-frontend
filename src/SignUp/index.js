9bc3dc64ef1314a7d16d2fdbbe3f2992b267a730
brunoPertini <brunopertini@gmail.com>
2023-03-03 17:07:40 -0300

    Se agregaron mas campos al formulario de registro

--- src/SignUp/index.js
+++ src/SignUp/index.js
@@ -1,20 +1,59 @@
 import {
-  Grid, FormControl, TextField,
+  Grid, TextField, Typography,
 } from '@mui/material';
 import Header from '../Header';
 
 export default function UserSignUp() {
   return (
-    <Grid container sx={{ justifyContent: 'center' }}>
+    <Grid>
       <Header />
-      <Grid item sx={{ marginTop: '5%' }}>
-        <FormControl>
+      <Grid
+        container
+        sx={{
+          marginTop: '5%',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        spacing={2}
+      >
+        <Typography variant="h6" align="left">
+          Registrate! Sólo llevará unos minutos
+        </Typography>
+        <Grid item xs={8}>
           <TextField
             id="outlined-controlled"
             label="Nombre"
             onChange={() => {}}
           />
-        </FormControl>
+          <TextField
+            id="outlined-controlled"
+            label="Apellido"
+            onChange={() => {}}
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <TextField
+            id="outlined-controlled"
+            label="Email"
+            type="email"
+            onChange={() => {}}
+          />
+          <TextField
+            id="outlined-controlled"
+            label="Contraseña"
+            type="password"
+            onChange={() => {}}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="outlined-controlled"
+            label="Nacimiento"
+            type="date"
+            onChange={() => {}}
+            sx={{ width: '200%' }}
+          />
+        </Grid>
       </Grid>
     </Grid>
   );
