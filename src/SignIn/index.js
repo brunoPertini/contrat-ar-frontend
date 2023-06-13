d11c7a4565a42ec9ef3c7335d4dc745892bba656
brunoPertini <brunopertini@gmail.com>
2023-06-13 16:59:54 -0300

    Improves en la pagina de login, logica para los campos vacios  del formulario

--- src/SignIn/index.js
+++ src/SignIn/index.js
@@ -1,4 +1,4 @@
-// import Grid from '@mui/material/Grid';
+import { useMemo, useState } from 'react';
 import Header from '../Header';
 import { SignInFormBuilder } from '../Shared/Helpers/FormBuilder';
 import { Form } from '../Shared/Components';
@@ -6,12 +6,50 @@ import { signinLabels } from '../StaticData/SignIn';
 
 function SignIn() {
   const formBuilder = new SignInFormBuilder();
+  const [formValues, setFormValues] = useState(formBuilder.fields);
+  const [emptyFields, setEmptyFields] = useState([]);
+
+  const onButtonClick = () => {
+    let newErrorFields = [...emptyFields];
+    Object.keys(formValues).forEach((key) => {
+      if (!formValues[key]) {
+        newErrorFields.push(key);
+      } else {
+        newErrorFields = newErrorFields.filter((element) => element !== key);
+      }
+    });
+
+    setEmptyFields(newErrorFields);
+  };
+
+  const errorMessage = useMemo(() => {
+    if (emptyFields.length) {
+      return 'Por favor, revisá los campos vacíos';
+    }
+
+    if (false) {
+      return 'Usuario o contraseña incorrectos';
+    }
+
+    return '';
+  }, [emptyFields]);
+
+  const formFields = formBuilder.build({
+    fieldsValues: formValues,
+    onChangeFields: (fieldId, fieldValue) => {
+      setFormValues({ ...formValues, [fieldId]: fieldValue });
+    },
+    errorFields: emptyFields,
+    onButtonClick,
+    errorMessage,
+  });
+
   return (
     <>
       <Header />
       <Form
         title={signinLabels.title}
-        fields={formBuilder.build()}
+        fields={formFields}
       />
 
     </>
