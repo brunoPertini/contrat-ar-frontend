dba3e1f238ac1b1b4aa180076b7c02b6c81bec4c
brunoPertini <brunopertini@gmail.com>
2023-07-04 15:01:36 -0300

    cambios minimos

--- src/Shared/Components/VendiblesList.js
+++ src/Shared/Components/VendiblesList.js
@@ -29,66 +29,38 @@ export default function VendiblesList({ items }) {
   );
 }
 
-// {
-//   proveedor: {
-//     name: 'Bruno ',
-//     surname: 'Pertini',
-//     location: {
-//       coordinates: [
-//         34.9474625, -57.9733943,
-//       ],
-//       distanceFrom: 20,
-//     },
-//   },
-//   services: [
-//     {
-//       title: 'Veterinario',
-//       text: 'jkdfjkdjfkdjfkdjflkjlkjlkjlkjlkjlkjkljlkjkljkljkljlkjkljlkjlkjlkjlkj',
-//       image: {
-//         src: 'https://images.unsplash.com/photo-1544197807-bb503430e22d?ixlib=rb-4.0.3&dpr=1&auto=format&fit=crop&q=60&w=400&h=400',
-//       },
-//     },
-//     {
-//       title: 'Relojero',
-//       text: 'gfdlkjgdflkgjdflkgjdflkgjdfgfgfdgggdgd',
-//       image: {
-//         src: 'https://images.unsplash.com/photo-1541480601022-2308c0f02487?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60&h=400',
-//       },
-//     },
-//   ],
-// },
+// TODO: extraerlo cuando se integre el backend
+// const proveedorShape = PropTypes.shape({
+//   name: PropTypes.string,
+//   surname: PropTypes.string,
+//   location: PropTypes.shape({
+//     coordinates: PropTypes.arrayOf(PropTypes.number),
+//     distanceFrom: PropTypes.number,
+//   }),
+// });
 
-const proveedorShape = PropTypes.shape({
-  name: PropTypes.string,
-  surname: PropTypes.string,
-  location: PropTypes.shape({
-    coordinates: PropTypes.arrayOf(PropTypes.number),
-    distanceFrom: PropTypes.number,
-  }),
-});
+// const servicioShape = PropTypes.shape({
+//   proveedor: PropTypes.shape(proveedorShape).isRequired,
+//   services: PropTypes.arrayOf(PropTypes.shape({
+//     title: PropTypes.string,
+//     text: PropTypes.string,
+//     image: PropTypes.shape({
+//       src: PropTypes.string,
+//     }).isRequired,
+//   })),
+// });
 
-const servicioShape = PropTypes.shape({
-  proveedor: PropTypes.shape(proveedorShape).isRequired,
-  services: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    text: PropTypes.string,
-    image: PropTypes.shape({
-      src: PropTypes.string,
-    }).isRequired,
-  })),
-});
-
-const productoShape = PropTypes.shape({
-  proveedor: PropTypes.shape(proveedorShape).isRequired,
-  products: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    text: PropTypes.string,
-    image: PropTypes.shape({
-      src: PropTypes.string,
-    }).isRequired,
-  })),
-});
+// const productoShape = PropTypes.shape({
+//   proveedor: PropTypes.shape(proveedorShape).isRequired,
+//   products: PropTypes.arrayOf(PropTypes.shape({
+//     title: PropTypes.string,
+//     text: PropTypes.string,
+//     image: PropTypes.shape({
+//       src: PropTypes.string,
+//     }).isRequired,
+//   })),
+// });
 
 VendiblesList.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.oneOfType([servicioShape, productoShape])).isRequired,
+  items: PropTypes.array.isRequired,
 };
