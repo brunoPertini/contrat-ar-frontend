fd213d931b7223abb384512c6fd7415b0c0fb8fd
brunoPertini <brunopertini@gmail.com>
2023-09-19 17:11:57 -0300

    Improves en pagina de  vendible

--- src/Vendible/Components/index.js
+++ src/Vendible/Components/index.js
@@ -1,11 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/prop-types */
 import Grid from '@mui/material/Grid';
-import {
-  Avatar,
-  ImageListItem,
-  List, ListItem, ListItemAvatar, ListItemText, Typography,
-} from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import ImageListItem from '@mui/material/ImageListItem';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import PlaceIcon from '@mui/icons-material/Place';
 import { Link } from 'react-router-dom';
 import { useCallback } from 'react';
@@ -13,8 +18,10 @@ import Header from '../../Header';
 import { sharedLabels } from '../../StaticData/Shared';
 import { vendiblesLabels } from '../../StaticData/Vendibles';
 import { systemConstants } from '../../Shared/Constants';
+import { labels as clientLabels } from '../../StaticData/Cliente';
+import UserAccountOptions from '../../Shared/Components/UserAccountOptions';
 
-function VendiblePage({ proveedoresInfo, vendibleType }) {
+function VendiblePage({ proveedoresInfo, vendibleType, userInfo }) {
   const getPriceLabel = useCallback((price) => {
     if (vendibleType === systemConstants.PRODUCTS) {
       return price === 0 ? sharedLabels.priceToBeAgreed : `${sharedLabels.price}${price}`;
@@ -23,9 +30,14 @@ function VendiblePage({ proveedoresInfo, vendibleType }) {
     return price === 0 ? sharedLabels.priceToBeAgreed : `${sharedLabels.minimalPrice}${price}`;
   }, [vendibleType]);
 
+  const menuOptions = [{
+    component: UserAccountOptions,
+    props: { userInfo },
+  }];
+
   return (
     <>
-      <Header withMenuComponent />
+      <Header withMenuComponent menuOptions={menuOptions} />
       <Grid
         container
         sx={{
@@ -53,60 +65,69 @@ function VendiblePage({ proveedoresInfo, vendibleType }) {
                 const fullName = `${name} ${surname}`;
 
                 return (
-                  <ListItem alignItems="flex-start" divider>
-                    <ListItemAvatar>
-                      <Avatar
-                        alt={fullName}
-                        src={fotoPerfilUrl}
+                  <>
+                    <ListItem alignItems="flex-start">
+                      <ListItemAvatar>
+                        <Avatar
+                          alt={fullName}
+                          src={fotoPerfilUrl}
+                          sx={{
+                            height: 100,
+                            width: 100,
+                          }}
+                        />
+                        <ListItemText primary={fullName} />
+                        {!!distanceFrom && (
+                        <>
+                          <Typography variant="body2" color="text.secondary">
+                            {sharedLabels.to}
+                            {' '}
+                            {distanceFrom}
+                            {' '}
+                            {sharedLabels.kilometersAway}
+                            <PlaceIcon fontSize="medium" />
+                          </Typography>
+                          <Link href="#">
+                            {vendiblesLabels.seeInMap}
+                          </Link>
+                        </>
+                        )}
+                      </ListItemAvatar>
+                      <ImageListItem
                         sx={{
-                          height: 100,
-                          width: 100,
+                          width: '50%',
+                          display: 'flex',
+                          flexDirection: 'column',
+                          alignItems: 'center',
                         }}
-                      />
-                      <ListItemText primary={fullName} />
-                      {
-                        !!distanceFrom && (
-                          <>
-                            <Typography variant="body2" color="text.secondary">
-                              {sharedLabels.to}
-                              {' '}
-                              {distanceFrom}
-                              {' '}
-                              {sharedLabels.kilometersAway}
-                              <PlaceIcon fontSize="medium" />
-                            </Typography>
-                            <Link href="#">
-                              {vendiblesLabels.seeInMap}
-                            </Link>
-                          </>
-                        )
-                      }
-                    </ListItemAvatar>
-                    <ImageListItem
-                      sx={{
-                        width: '50%',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                      }}
-                      key={info.imagenUrl}
-                    >
-                      <img
-                        src={info.imagenUrl}
-                        srcSet={info.imagenUrl}
-                        alt={info.proveedorInfo.name}
-                        loading="lazy"
-                      />
-                      <ListItemText
-                        primary={info.descripcion}
-                      />
-                    </ImageListItem>
-                    <ListItem>
-                      <Typography variant="h5">
-                        { getPriceLabel(precio) }
-                      </Typography>
+                        key={info.imagenUrl}
+                      >
+                        <img
+                          src={info.imagenUrl}
+                          srcSet={info.imagenUrl}
+                          alt={info.proveedorInfo.name}
+                          loading="lazy"
+                        />
+                        <ListItemText
+                          primary={info.descripcion}
+                        />
+                      </ImageListItem>
+                      <ListItem sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <Typography variant="h5">
+                          {getPriceLabel(precio)}
+                        </Typography>
+                        <Typography variant="h5" sx={{ mt: '50px' }}>
+                          {clientLabels.contactProvider.replace('{nombreProveedor}', name)}
+                        </Typography>
+                        <TextareaAutosize minRows={5} cols={50} />
+                        <Button variant="contained" sx={{ mt: '5px' }}>
+                          Enviar mensaje
+                        </Button>
+                      </ListItem>
                     </ListItem>
-                  </ListItem>
+                    <Divider variant="outlined" sx={{ borderColor: 'black' }} />
+
+                  </>
                 );
               })
             }
--- src/Vendible/Containers/index.js
+++ src/Vendible/Containers/index.js
@@ -1,11 +1,29 @@
 import { useLocation } from 'react-router-dom';
+import { createSelector } from 'reselect';
+import { useSelector } from 'react-redux';
 import VendiblePage from '../Components';
+import { withRouter } from '../../Shared/Components';
+
+const stateSelector = (state) => state;
 
 function VendibleContainer() {
   const location = useLocation();
-  const { proveedoresInfo } = location.state;
+  const { proveedoresInfo, vendibleType } = location.state;
 
-  return <VendiblePage proveedoresInfo={proveedoresInfo} />;
+  const userInfoSelector = createSelector(
+    stateSelector,
+    (state) => state.usuario,
+  );
+
+  const userInfo = useSelector(userInfoSelector);
+
+  return (
+    <VendiblePage
+      proveedoresInfo={proveedoresInfo}
+      vendibleType={vendibleType}
+      userInfo={userInfo}
+    />
+  );
 }
 
-export default VendibleContainer;
+export default withRouter(VendibleContainer);
