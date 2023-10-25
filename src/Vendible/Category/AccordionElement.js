55a88ced62d3de75ea283ed062bfb1f40a443799
brunoPertini <brunopertini@gmail.com>
2023-10-25 16:24:15 -0300

    Renderizando niveles mas anidados de categorias

--- src/Vendible/Category/AccordionElement.js
+++ src/Vendible/Category/AccordionElement.js
@@ -2,6 +2,8 @@ import Typography from '@mui/material/Typography';
 import Accordion from '@mui/material/Accordion';
 
 class AccordionElement {
+  #rootName;
+
   #root;
 
   #children;
@@ -10,21 +12,27 @@ class AccordionElement {
 
   #onChange;
 
-  constructor(root, children, isSuperCategory, onChange) {
+  constructor(rootName, root, children, isSuperCategory, onChange) {
     this.#root = root;
     this.#children = children;
     this.#isSuperCategory = isSuperCategory;
     this.#onChange = onChange;
+    this.#rootName = rootName;
   }
 
   get root() {
     return this.#root;
   }
 
+  get rootName() {
+    return this.#rootName;
+  }
+
   set children(children) {
     this.#children = children;
   }
 
+  /** @returns {Array<AccordionElement>} */
   get children() {
     return this.#children;
   }
@@ -36,7 +44,7 @@ class AccordionElement {
                 this.#root
         }
         {
-                this.#children
+                this.#children?.length ? this.children.map((c) => c.render()) : null
         }
       </Accordion>
     ) : <Typography>{ this.#root }</Typography>;
--- src/Vendible/Category/CategoryAccordion.js
+++ src/Vendible/Category/CategoryAccordion.js
@@ -1,53 +1,64 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-unreachable-loop */
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import isEmpty from 'lodash/isEmpty';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AccordionSummary from '@mui/material/AccordionSummary';
-import Accordion from '@mui/material/Accordion';
 import { vendiblesLabels } from '../../StaticData/Vendibles';
 import AccordionElement from './AccordionElement';
 
 function CategoryAccordion({ categories, vendibleType }) {
-  const [categoriesSubSection, setCategoriesSubSection] = useState({});
+  const [categoriesSubSection, setCategoriesSubSection] = useState();
 
-  const handleAccordionClick = ({ root, children }) => (_, isExpanded) => {
+  /**
+   * @param {AccordionElement} categoryTree
+   * @param {String} categoryName
+   * @returns { AccordionElement} */
+  function searchCategoryInTree(categoryTree, categoryName) {
+    if (categoryTree.rootName === categoryName) {
+      return categoryTree;
+    }
+    if (categoryTree.children?.length) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const child of categoryTree.children) {
+        const found = searchCategoryInTree(child, categoryName);
+        return found || null;
+      }
+    }
+
+    return null;
+  }
+
+  const handleAccordionClick = ({ root, children }) => (_, isExpanded) => setCategoriesSubSection((currentCategories) => {
+    const updatedCategories = [...currentCategories];
+    let toUpdateElement = null;
+    for (const currentTree of updatedCategories) {
+      const r = searchCategoryInTree(currentTree, root);
+      if (r) {
+        toUpdateElement = r;
+      }
+    }
     if (isExpanded) {
-      const isSuperCategory = !!(children.length);
-
-      const rootJsx = (
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography>{ root }</Typography>
-        </AccordionSummary>
-      );
-
-      const childrenJsx = children.map(({ root: childRoot, children: newChildren }) => (
-
-        <Accordion onChange={handleAccordionClick({
-          root: childRoot,
-          children: newChildren,
-        })}
-        >
+      children.forEach(({ root: childRoot, children: newChildren }) => {
+        const childrenRootJsx = (
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
             <Typography>{ childRoot }</Typography>
           </AccordionSummary>
-        </Accordion>
-      ));
+        );
+        const onChangeChildren = () => handleAccordionClick({ root: childRoot, children: newChildren });
 
-      const onChange = () => handleAccordionClick({ root, children });
-
-      categoriesSubSection[root]
-
-      const accordionElement = new AccordionElement(rootJsx, childrenJsx, isSuperCategory, onChange);
-
-      setCategoriesSubSection((previous) => ({ ...previous, [root]: accordionElement }));
-    } else {
-      setCategoriesSubSection((previous) => ({ ...previous, [root]: null }));
+        toUpdateElement.children.push(new AccordionElement(childRoot, childrenRootJsx, [], !!(newChildren.length), onChangeChildren));
+      });
+      return updatedCategories;
     }
-  };
+    return [];
+  });
 
   useEffect(() => {
+    const firstCategoriesSections = [];
     if (!isEmpty(categories)) {
       Object.keys(categories).forEach((rootCategoryName) => {
         const { root, children } = categories[rootCategoryName];
@@ -64,25 +75,25 @@ function CategoryAccordion({ categories, vendibleType }) {
 
         const onChange = () => handleAccordionClick({ root, children });
 
-        const accordionElement = new AccordionElement(rootJsx, childrenJsx, isSuperCategory, onChange);
+        const accordionElement = new AccordionElement(root, rootJsx, childrenJsx, isSuperCategory, onChange);
 
-        setCategoriesSubSection((previous) => ({ ...previous, [root]: accordionElement }));
+        firstCategoriesSections.push(accordionElement);
       });
+
+      setCategoriesSubSection(firstCategoriesSections);
     }
   }, [categories]);
 
   if (!isEmpty(categoriesSubSection)) {
     const categoriesTitle = vendiblesLabels.categoryOfVendible.replace('{vendibleType}', vendibleType);
+    console.log(categoriesSubSection);
     return (
       <div>
         <Typography variant="h4">
           { categoriesTitle }
         </Typography>
         {
-          Object.values(categoriesSubSection).map((accordionElement) => {
-            console.log(accordionElement);
-            return accordionElement.render();
-          })
+          categoriesSubSection.map((accordionElement) => accordionElement.render())
         }
       </div>
     );
