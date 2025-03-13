ea1ce4883666b12303e4dd929b52427f0264f1cb
bruno <brunopertini@gmail.com>
2025-03-13 15:34:43 -0300

    Todos los flujos terminados

--- src/SignIn/Components/RestorePasswordPage.js
+++ src/SignIn/Components/RestorePasswordPage.js
@@ -105,7 +105,7 @@ export default function RestorePasswordPage() {
       </Box>
       ) }
       {
-        isTokenValid && wasUpdateSuccessful === null && (
+        isTokenValid && wasUpdateSuccessful === null && !isLoading && (
         <SecurityData
           data={data}
           setData={handleDataChanged}
